61e9f45ecf9cf4f448869f285a9930b530094772
Rafaelovanegas <rafaelovanegas@gmail.com>
2025-12-26 23:56:43 -0600

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,14 @@
+
+export enum ToolType {
+  DASHBOARD = 'DASHBOARD',
+  IMAGE = 'IMAGE',
+  TEXT = 'TEXT',
+  AUDIO = 'AUDIO',
+  LIVE = 'LIVE'
+}
+
+export interface TranscriptionEntry {
+  role: 'user' | 'model';
+  text: string;
+  timestamp: number;
+}
